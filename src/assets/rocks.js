39cf9c9677ed426d47f56b6a7b8067201beee131
bigVL <84589629+BIGVl@users.noreply.github.com>
2022-04-22 17:54:20 +0300

    Added the rocks on the page

---
+++ src/assets/rocks.js
@@ -0,0 +1,18 @@
+import rock1 from './rocks/rock1.jpeg';
+import rock2 from './rocks/rock2.jpeg';
+import rock3 from './rocks/rock3.jpeg';
+import rock4 from './rocks/rock4.jpeg';
+import rock5 from './rocks/rock5.jpeg';
+import rock6 from './rocks/rock6.jpeg';
+import rock7 from './rocks/rock7.jpeg';
+import rock8 from './rocks/rock8.jpeg';
+import rock9 from './rocks/rock9.jpeg';
+import rock10 from './rocks/rock10.jpeg';
+import rock11 from './rocks/rock11.jpeg';
+import rock13 from './rocks/rock13.jpeg';
+import rock14 from './rocks/rock14.jpeg';
+import rock15 from './rocks/rock15.jpeg';
+import rock16 from './rocks/rock16.jpeg';
+
+const rocks = [rock1, rock2, rock3, rock4, rock5, rock6, rock7, rock8, rock9, rock10, rock11, rock13, rock14, rock15, rock16];
+export default rocks;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import rocks from '../assets/rocks';
+import '../styles/Shop.css';
 
 const Shop = () => {
   const [sneakersImgs, setSneakerImgs] = useState([]);
+
+  useEffect(() => {
+    setSneakerImgs(rocks);
+  }, []);
 
   return (
     <div className="shop-container">
       {sneakersImgs.map((sneakerImg) => {
         if (sneakerImg !== '') {
-          return <img src={sneakerImg} alt="" />;
+          return <img className="rock" src={sneakerImg} alt="" />;
         }
       })}
     </div>
