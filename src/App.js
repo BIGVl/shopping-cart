87bd06cdbef1d483adb3b6324d8ec21f46dbe94a
bigVL <84589629+BIGVl@users.noreply.github.com>
2022-05-03 18:23:42 +0300

    Finish deployment

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import React, { useState } from 'react';
 import Shop from './components/Shop';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
   const [xItems, setxItems] = useState(0);
 
   return (
-    <Router basename="/Rocks">
+    <Router>
       <Navbar xItems={xItems} />
       <Routes>
         <Route path="/" element={<Home />} />
